a891aec7941ae238319c3190dd89119c89606029
Catmanman <148367781+Catmanman@users.noreply.github.com>
2023-10-18 18:31:21 +0200

    Create myclass.java

---
+++ lecture 3/myclass.java
@@ -0,0 +1,36 @@
+package David Najdovski 5446;
+
+public class myNumber {
+	private int num;
+	public int num2;
+	public myNumber() {
+		num=0;
+		num2=100;
+	}
+	int getNum() {
+		return num;
+	}
+	void setNum(int i) {
+		num=i;
+		num2=10*i;
+	}
+	int getNum2() {
+		return num2;
+	}
+	void description() {
+		System.out.println("The value of num is" +  this.getNum());
+		System.out.println("The value of num2 is" +  this.getNum2());
+	}
+	float getAverage() {
+		return (num+num2/2);
+}
+}
+public class myClass {
+	public static void main(String[] args) {
+		myNumber A = new myNumber();
+		System.out.println(A.getNum());
+		A.setNum(10);
+		System.out.println(A.getNum());
+		System.out.println(A.num2);
+	}
+}
